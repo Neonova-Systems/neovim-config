844485be0a4f3b217245f5e4c12837164b59fdb6
Xaxeric <xaxeric.dev@gmail.com>
2026-07-02 10:48:24 +0700

    change(colorscheme): enable transparent option

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,6 +3,12 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+        require('alabaster').setup({
+            style = "dark",          -- "light" or "dark"
+            transparent = true,      -- Enable transparent background
+            italic_comments = false,  -- Enable italic comments
+        })
+
         vim.cmd.colorscheme('alabaster')
     end,
 }
