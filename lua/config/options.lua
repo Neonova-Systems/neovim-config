cf2aba25a44917d2d98aa92fd04dd9ec42b119c5
Xaxeric <xaxeric.dev@gmail.com>
2026-07-02 10:13:19 +0700

    test: cmdline

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,5 @@
 vim.opt.wrap = false                                                -- Changes how text is displayed to not wrap
-vim.opt.binary = true                                               -- This option should be set before editing a binary file
+vim.opt.binary = false                                               -- This option should be set before editing a binary file
 vim.opt.title = true                                                -- When on, the title of the window will be set to the value of 'titlestring' (if it is not empty), or to: filename [+=-] (path) - NVIM
 vim.opt.number = true                                               -- Enable line numbers
 vim.opt.relativenumber = true                                       -- Enable relative line numbers
@@ -12,7 +12,16 @@ vim.opt.smartindent = true                                          -- Do smart 
 vim.opt.list = false                                                -- List mode: By default, show tabs as ">", trailing spaces as "-", and non-breakable space characters as "+". Useful to see the difference between tabs and spaces and for trailing blanks.
 vim.opt.scrolloff = 10                                              -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.sidescrolloff = 10                                          -- The minimal number of screen columns to keep to the left and to the right of the cursor if 'nowrap' is set.
-vim.opt.wildmenu = false                                             -- Enable 'enchanced mode' of command-line completion.
+vim.opt.wildmenu = true                                             -- Enable 'enchanced mode' of command-line completion.
+-- Force explicit character injection on backspace events
+vim.keymap.set("c", "<BS>", function()
+  return vim.fn.getcmdpos() > 1 and "<BS>" or ""
+end, { expr = true, remap = false })
+
+-- Override default terminal blockades
+vim.keymap.set("c", "<C-h>", function()
+  return vim.fn.getcmdpos() > 1 and "<BS>" or ""
+end, { expr = true, remap = false })
 vim.opt.wildmode = "full"                                           -- Completion mode that is used for the character specified with 'wildchar'.
 vim.opt.wildignore = {'**/.git/*', '**/node_modules/*'}             -- A list of file patterns, a file that matches with one of these patterns is ignored when expanding wildcards.
 vim.opt.showcmd = true                                              -- Show (partial) command in the last line of the screen. Set this option off if your terminal is slow.
