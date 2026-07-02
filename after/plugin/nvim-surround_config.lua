3c967d71ab50dbb1cafe75dd591aef925fa7a856
Xaxeric <xaxeric.dev@gmail.com>
2026-07-02 11:34:50 +0700

    purge unused configs

--- after/plugin/nvim-surround_config.lua
+++
@@ -1,5 +0,0 @@
-if not pcall(require, "nvim-surround") then -- Check if nvim has nvim-surround plugin
-    return -- Stop sourcing this file.
-end -- End if-else statement
-
-require("nvim-surround").setup({ }) -- Configuration here, or leave empty to use defaults
